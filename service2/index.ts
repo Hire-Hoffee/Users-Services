a9a646959d82cffaa066527e569b94fd85f4515a
Rostislav Alekseev <mr.rostik.01@gmail.com>
2023-10-14 17:19:33 +0200

    service2 setup

--- service2/index.ts
+++ service2/index.ts
@@ -1,1 +1,13 @@
-console.log("Users history service");
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 6666;
+
+app.use(express.json());
+
+app.use("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
