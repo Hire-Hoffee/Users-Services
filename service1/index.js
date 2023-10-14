7b01fe9ef2c6f18bd34ac1f57191c6c0d076295c
Rostislav Alekseev <mr.rostik.01@gmail.com>
2023-10-14 15:48:59 +0200

    first service setup

--- service1/index.js
+++ service1/index.js
@@ -1,1 +1,12 @@
-console.log("Users service");
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.PORT || 5555;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
